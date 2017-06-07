b119a01553becd648bcf0759987cbcf43ca14457
Petersoj <petersojj@gmail.com>
2017-06-06 18:30:11 -0600

    Initial Project

---
+++ src/main/java/me/petersoj/PluginIdeaGenerator.java
@@ -0,0 +1,11 @@
+package me.petersoj;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class PluginIdeaGenerator extends JavaPlugin {
+	
+	@Override
+	public void onEnable() {
+		
+	}
+}
---
+++ src/main/java/me/petersoj/idea/PluginIdeas.java
@@ -0,0 +1,8 @@
+package me.petersoj.idea;
+
+public class PluginIdeas {
+	
+	public String getNewIdea(){
+		return "";
+	}
+}
